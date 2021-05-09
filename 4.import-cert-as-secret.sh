647767fd0f8f1dab1a870f9b28dee7f3c7b07739
tanmayakp <tanmaya.cs@gmail.com>
2021-05-09 16:33:35 +0530

    Adding self signed certificates

--- 4.import-cert-as-secret.sh
+++ 4.import-cert-as-secret.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-kubectl create -n istio-system secret tls havefish-creds --key=IstioConfigs/server.key --cert=IstioConfigs/www_havefish_ml.crt
+#kubectl create -n istio-system secret tls havefish-creds --key=IstioConfigs/server.key --cert=IstioConfigs/www_havefish_ml.crt
+openssl req -x509 -newkey rsa:2048 -subj '/CN=*.havefish.ml'  -keyout IstioConfigs/key.pem -out IstioConfigs/cert.pem -days 365 -nodes
+
+kubectl create -n istio-system secret tls havefish-creds --key=IstioConfigs/key.pem --cert=IstioConfigs/cert.pem
